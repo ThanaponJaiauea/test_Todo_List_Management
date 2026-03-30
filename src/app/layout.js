f2c8cf851c2e22e1adbba4f3857561d740b997ea
ThanaponJaiauea <162958446+ThanaponJaiauea@users.noreply.github.com>
2026-03-30 12:39:36 +0700

    feat: initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">     <Providers>{children}</Providers></body>
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
